2cf3407b27da68d34e57e070fbd01242ec8c07c0
ZARI <thezah50@gmail.com>
2026-08-03 02:53:30 +0330

    update service

--- rag/service.py
+++ rag/service.py
@@ -2,27 +2,27 @@ from rag.retriever import search
 from rag.prompt_builder import build_prompt
 from rag.llm import generate
 
+DEBUG = True
+
 
 def ask(question):
-
     documents = search(question)
 
-    print("="*50)
-    print("Documents:", len(documents))
-    print("="*50)
-
-    for doc in documents:
-        print(doc.metadata["name"])
+    if DEBUG:
+        print("=" * 50)
+        print("Relevant documents:", len(documents))
+        for doc in documents:
+            print("-", doc.metadata["name"])
+        print("=" * 50)
 
     if not documents:
         return "این اطلاعات در محصولات فروشگاه موجود نیست."
 
     prompt = build_prompt(question, documents)
 
-    print("="*50)
-    print(prompt)
-    print("="*50)
+    if DEBUG:
+        print(prompt)
+        print("=" * 50)
 
     answer = generate(prompt)
-
     return answer
