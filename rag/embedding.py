43d38f4bda1b35758e0e537c3068c2909284e6e5
ZARI <thezah50@gmail.com>
2026-08-03 02:49:03 +0330

    upadate embedding file

--- rag/embedding.py
+++ rag/embedding.py
@@ -1,9 +1,10 @@
+import os
+
+os.environ["HF_HUB_OFFLINE"] = "1"
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+
 from langchain_huggingface import HuggingFaceEmbeddings
 
 embedding_model = HuggingFaceEmbeddings(
     model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
-)
-
-vector = embedding_model.embed_query(
-    "کیبورد مناسب برنامه نویسی"
 )
